924ea6a8adac524143ef4075bc39f24536fae2b3
Rafiu Moshood <meezyloaded@gmail.com>
2024-01-14 07:49:39 +0100

    learn: working with styling, vanilla css and modules

--- react-app/src/components/ListGroup/ListGroup.tsx
+++ react-app/src/components/ListGroup/ListGroup.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import styles from './ListGroup.module.css';
 
 interface Props {
 	items: string[];
@@ -13,7 +14,7 @@ export default function ListGroup({ items, heading, onSelectItem }: Props) {
 		<>
 			<h1>{heading}</h1>
 			{items.length === 0 && <p>No Item Found</p>}
-			<ul className="list-group">
+			<ul className={[styles.listGroup, styles.container].join(' ')}>
 				{items.map((item, index) => (
 					<li
 						key={item}
